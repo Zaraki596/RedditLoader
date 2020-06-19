27cd11b741ccf8f28737c60ed5ac4d7b6681d965
zaraki <rohansingh94117@gmail.com>
2020-06-19 16:33:33 +0530

    Added SecondActivity.kt for image preview

---
+++ app/src/main/java/com/example/redditloader/ui/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.redditloader.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.redditloader.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
